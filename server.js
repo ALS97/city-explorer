457aeb2dd9e2c59d0ae6bf744ec307dc06f16c27
Austin Smith <austinleesmith97@gmail.com>
2020-04-09 20:50:42 -0700

    server deploys

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 'use strict';
 
-// Load Environment Variables from the .env file
 require('dotenv').config();
-
-// Application Dependencies
 const express = require('express');
 const cors = require('cors');
-
-// Application Setup
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
@@ -16,4 +11,23 @@ app.get( '/test', (request, response) => {
   const name = request.query.name;
   response.send( `Hello` );
 });
+
+app.get('/geo', handleLocation);
+
+function handleLocation( request, response ) {
+  let city = request.query.city;
+
+  let locationData = require('./data/geo.json');
+  let location = new Location(city, locationData[0]);
+  response.json(location);
+}
+
+function Location(city, data) {
+  this.search_query = city;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
+}
+console.log('/geo');
+app.listen( PORT, () => console.log('Server up on', PORT));
   
