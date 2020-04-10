78f03ba42ab0c2b2cffe1df95e1ed53729e20799
Austin Smith <austinleesmith97@gmail.com>
2020-04-09 19:59:52 -0700

    added to server file to test

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
+
+app.get( '/test', (request, response) => {
+  const name = request.query.name;
+  response.send( `Hello` );
+});
+  
