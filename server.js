427594deaa65ecb9e9114ad47c8e20d922a35e57
Austin Smith <austinleesmith97@gmail.com>
2020-04-11 23:02:36 -0700

    lab 07 work

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
+const superagent = require('superagent');
 app.use(cors());
 
 app.get( '/test', (request, response) => {
@@ -12,14 +13,29 @@ app.get( '/test', (request, response) => {
   response.send( `Hello` );
 });
 
-app.get('/geo', handleLocation);
+app.get('/location', handleLocation);
 
 function handleLocation( request, response ) {
+  // https://us1.locationiq.com/v1/search.php?key=3a7a618904508a&q=seattle&format=json
+  // use library called 'superagent'
+  // let locationData = require('./data/geo.json');
   let city = request.query.city;
+  const url = "https://us1.locationiq.com/v1/search.php";
+  const queryStringParams = {
+    key: process.env.LOCATION_TOKEN,
+    q: city,
+    format: 'json',
+    limit:1,
+  };
+  superagent.get(url)
+    .query(queryStringParams)
+    .then( reply => {
+      let locationData = reply.body[0];
+      let location = new Location(city, locationData);
+      response.json(location);
+    });
 
-  let locationData = require('./data/geo.json');
-  let location = new Location(city, locationData[0]);
-  response.json(location);
+
 }
 
 function Location(city, data) {
